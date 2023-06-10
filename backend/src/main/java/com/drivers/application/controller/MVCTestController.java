48426467b791c30d2e1231600ac1235f17a96ac1
Hariharan Umapathi <smarthariharan28@gmail.com>
2023-06-10 23:26:44 +0530

    [TESTCODE] MVC Testing Controller updated

--- backend/src/main/java/com/drivers/application/controller/MVCTestController.java
+++ backend/src/main/java/com/drivers/application/controller/MVCTestController.java
@@ -1,12 +1,6 @@
 package com.drivers.application.controller;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,12 +9,19 @@ import jakarta.servlet.http.HttpServletResponse;
 public class MVCTestController {
         @GetMapping("/add")
         public ModelAndView add_numbers(HttpServletRequest request,HttpServletResponse response){
-            Integer i1=Integer.parseInt(request.getParameter("num1"));
-            Integer i2=Integer.parseInt(request.getParameter("num2"));
-            System.out.println(i1+i2);
-            ModelAndView mv=new ModelAndView();
+             ModelAndView mv=new ModelAndView();
+             Integer result=0;
+            try {
+                Integer i1=Integer.parseInt(request.getParameter("num1"));
+                Integer i2=Integer.parseInt(request.getParameter("num2"));
+                System.out.println(i1+i2);
+            } catch(NumberFormatException e){
+                Integer i1=0;
+                Integer i2=0;
+                result=i1+i2;
+            }
             mv.setViewName("DriverList");
-            mv.addObject("result", i1+i2);
+            mv.addObject("result", result);
             return mv;
         }
 }
