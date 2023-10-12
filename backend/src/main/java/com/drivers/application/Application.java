11178f7acaf2781ca7d18b7edb08bfa777939223
Hariharan Umapathi <smarthariharan28@gmail.com>
2023-10-12 19:21:24 +0530

    DRIVERAPP: Logger Test

--- backend/src/main/java/com/drivers/application/Application.java
+++ backend/src/main/java/com/drivers/application/Application.java
@@ -5,20 +5,26 @@ package com.drivers.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /* import org.springframework.context.ApplicationContext; */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
+		final Logger logger = LoggerFactory.getLogger(Application.class);
+		logger.info("Application Started");
 		/* ApplicationContext ctx = */ SpringApplication.run(Application.class, args);
 
-		/* System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-		String[] beanNames = ctx.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-		for (String beanName : beanNames) {
-			System.out.println(beanName);
-		} */
+		/*
+		 * System.out.println("Let's inspect the beans provided by Spring Boot:");
+		 * 
+		 * String[] beanNames = ctx.getBeanDefinitionNames();
+		 * Arrays.sort(beanNames);
+		 * for (String beanName : beanNames) {
+		 * System.out.println(beanName);
+		 * }
+		 */
 	}
-	
+
 }
