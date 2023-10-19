2ef78ba0826e03aba25d2f71a6b2008d40355a07
Hariharan Umapathi <smarthariharan28@gmail.com>
2023-10-19 21:44:57 +0530

    DRIVERAPP: Dependencies Update

--- src/main/java/com/drivers/application/Application.java
+++ src/main/java/com/drivers/application/Application.java
@@ -1,10 +1,7 @@
 package com.drivers.application;
 
-/* import java.util.Arrays; */
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/* import org.springframework.context.ApplicationContext; */
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,17 +11,8 @@ public class Application {
 	public static void main(String[] args) {
 		final Logger logger = LoggerFactory.getLogger(Application.class);
 		logger.info("Application Started");
-		/* ApplicationContext ctx = */ SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args);
 
-		/*
-		 * System.out.println("Let's inspect the beans provided by Spring Boot:");
-		 * 
-		 * String[] beanNames = ctx.getBeanDefinitionNames();
-		 * Arrays.sort(beanNames);
-		 * for (String beanName : beanNames) {
-		 * System.out.println(beanName);
-		 * }
-		 */
 	}
 
 }
