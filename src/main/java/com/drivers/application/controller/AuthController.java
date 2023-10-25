0683def1224c206ee78f795597afa695b7d7134a
Hariharan Umapathi <smarthariharan28@gmail.com>
2023-10-25 23:21:00 +0530

    Authentication Controller Fixed

--- src/main/java/com/drivers/application/controller/AuthController.java
+++ src/main/java/com/drivers/application/controller/AuthController.java
@@ -1,32 +1,31 @@
 package com.drivers.application.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.drivers.application.dao.request.SignUpRequest;
+import com.drivers.application.dao.request.SigninRequest;
+import com.drivers.application.dao.response.JwtAuthenticationResponse;
+import com.drivers.application.services.AuthenticationService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class AuthController {
-    @GetMapping("/login")
-    @ResponseBody
-    public String login() {
-        return "login";
-    }
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/driver/api/v1/auth")
     @ResponseBody
-    public Object authenticate(@RequestBody Map<String, Object> payload) {
-        System.out.println("Login Request Receiverd");
+    public ResponseEntity<JwtAuthenticationResponse> authenticate(SigninRequest signinRequest) {
+        return ResponseEntity.ok(authenticationService.signin(signinRequest));
+    }
 
-        Map<String, Object> message = new HashMap<String, Object>();
-        String username = (String) payload.get("username");
-        String password = (String) payload.get("password");
-        message.put("message", "Login Request Received");
-        return message;
-
+    @PostMapping("/driver/api/v1/signup")
+    @ResponseBody
+    public ResponseEntity<JwtAuthenticationResponse> signup(SignUpRequest signupRequest) {
+        return ResponseEntity.ok(authenticationService.signup(signupRequest));
     }
 }
